08268dcae3a031c0ce9b3e7a18f35eecfae06ba2
Dyzer4 <dyegodasilvaroque86@gmail.com>
2025-11-06 23:02:05 -0300

    refactor: Candidates and Voter Models and configs

--- TIVIT.CIPA.Api.Domain/Model/Candidate.cs
+++ TIVIT.CIPA.Api.Domain/Model/Candidate.cs
@@ -4,28 +4,20 @@
     {
         public int Id { get; set; }
         public int ElectionId { get; set; }
-        public int? VoterId { get; set; }
-        public int? SiteId { get; set; }
-        public string CorporateId { get; set; }
-        public string Name { get; set; }
-        public DateTime AdmissionDate { get; set; }
-        public DateTime BirthDate { get; set; }
-        public string Area { get; set; }
-        public string Department { get; set; }
-        public string Site { get; set; }
+        public int VoterID { get; set; }
+        public int SiteId { get; set; }
         public byte[] PhotoBase64 { get; set; }
-        public string PhotoURL { get; set; }
         public string PhotoMimeType { get; set; }
-        public bool IsActive { get; set; }
+        public bool IsActive { get; set; } = true;
+
         public DateTime? CreateDate { get; set; }
         public string CreateUser { get; set; }
         public DateTime? UpdateDate { get; set; }
         public string UpdateUser { get; set; }
 
-        // Navigation Properties
-        public virtual Election Election { get; set; }
-        public virtual Voter Voter { get; set; }
-        public virtual Site SiteNavigation { get; set; }
+        public Election Election { get; set; }
+        public Voter Voter { get; set; }
+        public Site Site { get; set; }
         public virtual ICollection<Vote> Votes { get; set; }
     }
 }
